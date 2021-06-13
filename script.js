eb55a73b840994ab7d3125d9eb5dd257768f7715
Maryna Maksymchuk <risaranma@gmail.com>
2021-06-13 21:45:11 +0300

    Сделала урок 14

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
 'use strict';
 
-const books = document.querySelectorAll('.books'),
-      booksElem = document.querySelectorAll('.book'),
-      ulElems = document.querySelectorAll('ul'),
-      aElems = document.querySelectorAll('a'),
-      liElems = document.querySelectorAll('li');
-// 1)
-booksElem[1].after(booksElem[0]);
-booksElem[3].after(booksElem[5]);
-booksElem[5].after(booksElem[2]);
-booksElem[3].before(booksElem[4]);
+function DomElement (selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+}
 
+DomElement.prototype.newElem = function () {
+    let elem;
+    if (this.selector[0] === '.') {
+        elem = document.createElement('div');
+        elem.className = this.selector.slice(1);
+    }
+    if (this.selector[0] === '#') {
+        elem = document.createElement('p');
+        elem.id = this.selector.slice(1);
+        elem.textContent = 'До Нового Года осталось 200 дней';
+    }
+    elem.style.cssText = `height: ${this.height}px;
+        width: ${this.width}px;
+        background: ${this.bg};
+        font-size: ${this.fontSize}px;
+        margin: 200px 500px;
+        padding: 30px;
+        border-radius: 15px`;
+    return elem;
+};
 
-// 2)
-document.querySelector('body').style.backgroundImage = 'url(./image/you-dont-know-js.jpg)'; 
+let elDiv = new DomElement('.block', 100, 200, '#40E0D0', 12);
+let elParagraph = new DomElement('#best', 150, 400, '#7B68EE', 30);
 
-// 3)
-aElems[4].text = 'Книга 3. this и Прототипы Объектов';
-
-// 4)
-document.querySelector('.adv').remove();
-
-// 5)
-
-liElems[3].after(liElems[8]);
-liElems[3].after(liElems[6]);
-liElems[10].before(liElems[2]);
-
-const fiveLiElem = ulElems[5].querySelectorAll('li');
-fiveLiElem[1].after(fiveLiElem[9]);
-fiveLiElem[4].after(fiveLiElem[2]);
-fiveLiElem[7].after(fiveLiElem[5]);
-
-// 6)
-
-ulElems[2].append('Глава 8: За пределами ES6');
-
-const sixliElems = ulElems[2].querySelectorAll('li');
-ulElems[2].append(sixliElems[9]);
+document.body.appendChild(elDiv.newElem());
+document.body.appendChild(elParagraph.newElem());
