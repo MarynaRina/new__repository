1cd19004cec056b41d5344e25f7332f1f9da99d9
Maryna Maksymchuk <risaranma@gmail.com>
2021-06-13 21:57:21 +0300

    Сделала урок 14

--- script.js
+++ script.js
@@ -1,37 +1,77 @@
 'use strict';
+document.addEventListener('DOMContentLoaded', function (event) {
+    const px = 10;
 
-function DomElement (selector, height, width, bg, fontSize) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-}
+    const DomElement = function (selector = '.block', styleAtr = {}) {
+        this.selector = selector === 'string' ? selector : (() => {
+            styleAtr = typeof selector === 'object' ? selector : {};
+            return '.block';
+        })();
+        this.height = '100px';
+        this.width = '100px';
+        this.bg = 'green';
+        this.fontSize = '10px';
+        this.cssText = '';
 
-DomElement.prototype.newElem = function () {
-    let elem;
-    if (this.selector[0] === '.') {
-        elem = document.createElement('div');
-        elem.className = this.selector.slice(1);
-    }
-    if (this.selector[0] === '#') {
-        elem = document.createElement('p');
-        elem.id = this.selector.slice(1);
-        elem.textContent = 'До Нового Года осталось 200 дней';
-    }
-    elem.style.cssText = `
-        height: ${this.height}px;
-        width: ${this.width}px;
-        background: ${this.bg};
-        font-size: ${this.fontSize}px;
-        margin: 200px 700px;
-        padding: 30px;
-        border-radius: 15px`;
-    return elem;
-};
+        if (!('height' in styleAtr)) {
+            styleAtr.height = '100px';
+        }
+        if (!('width' in styleAtr)) {
+            styleAtr.width = '100px';
+        }
+        if (!('background' in styleAtr)) {
+            styleAtr.background = '#B3F282';
+        }
+        if (!('font-size' in styleAtr)) {
+            styleAtr['font-size'] = '10px';
+        }
 
-let elDiv = new DomElement('.block', 100, 200, '#40E0D0', 12);
-let elParagraph = new DomElement('#best', 150, 400, '#7B68EE', 30);
+        for (let key in styleAtr) {
+            this.cssText += `${key}: ${styleAtr[key]}; `;
+        }
+    };
+    
+    DomElement.prototype.newElem = function () {
+        let elem;
+        
+        if (this.selector[0] === '.') {
+            elem = document.createElement('div');
+            elem.className = this.selector.slice(1);
+        }
+        
+        if (this.selector[0] === '#') {
+            elem = document.createElement('p');
+            elem.id = this.selector.slice(1);
+        }
+        
+        elem.style.cssText = this.cssText;
+        elem.style.cssText += `left: ${Math.round(window.innerWidth / 2 - parseInt(this.width) / 2)}px; `;
+        elem.style.cssText += `top: ${Math.round(window.innerHeight / 2 - parseInt(this.height) / 2)}px; `;
+        
+        elem.addEventListener('click', this.transfer);
+        
+        return elem;
+    };
+    
+    let square = new DomElement({
+        position: 'absolute'
+    });
 
-document.body.appendChild(elDiv.newElem());
-document.body.appendChild(elParagraph.newElem());
+    document.body.appendChild(square.newElem());
+    
+    document.addEventListener('keyup', (event) => {
+        const div = document.querySelector('div');
+        if (event.key === 'ArrowUp') {
+            div.style.top = parseInt(div.style.top) - px + 'px';
+        }
+        if (event.key === 'ArrowRight') {
+            div.style.left = parseInt(div.style.left) + px + 'px';
+        }
+        if (event.key === 'ArrowLeft') {
+            div.style.left = parseInt(div.style.left) - px + 'px';
+        }
+        if (event.key === 'ArrowDown') {
+            div.style.top = parseInt(div.style.top) + px + 'px';
+        }
+    });
+});
